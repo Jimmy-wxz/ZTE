fea977396c60aecf718d71c8d931e05a1a0f2832
Jimmy-wxz <xwudo@connect.ust.hk>
2026-07-06 16:13:03 +0800

    Upload recursive/knowledge_base/__init__.py

---
+++ recursive/knowledge_base/__init__.py
@@ -0,0 +1,4 @@
+from .service import KnowledgeBaseService
+from .constants import DEFAULT_EMBEDDING_MODEL, DEFAULT_KB_BASE_PATH
+
+__all__ = ["KnowledgeBaseService", "DEFAULT_EMBEDDING_MODEL", "DEFAULT_KB_BASE_PATH"]
